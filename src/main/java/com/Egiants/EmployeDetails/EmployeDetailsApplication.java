6af5f3fe2fd6cee70a2e60dfa18d28a80eef84f8
kryshnasdv <imgottapusaikrishna@gmail.com>
2018-05-04 12:38:57 -0500

    hii

---
+++ src/main/java/com/Egiants/EmployeDetails/EmployeDetailsApplication.java
@@ -0,0 +1,12 @@
+package com.Egiants.EmployeDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeDetailsApplication.class, args);
+	}
+}
